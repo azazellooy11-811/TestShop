e68636ce3e7465b03d5b8120b6f7b6225a877e73
azazellooy11-811 <khismatova_05@mail.ru>
2024-07-18 16:31:52 +0500

    created a new project

---
+++ TestShop/TestShop/TestShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestShopApp.swift
+//  TestShop
+//
+//  Created by Азалия Халилова on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
